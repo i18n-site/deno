9c1761e09ee6f76aa0a80988d11c9020f384ce4a
i18n <i18n.site@gmail.com>
2025-02-17 10:35:08 +0800

    retry@0.1.1

--- retry/mod.js
+++ retry/mod.js
@@ -1,1 +1,13 @@
-export default () => {};
+export default (func, times = 3) => async (...args) => {
+  let n = 0;
+  while (1) {
+    try {
+      return await func(...args);
+    } catch (e) {
+      if (times == n++) {
+        throw e;
+      }
+      console.error(e);
+    }
+  }
+};
