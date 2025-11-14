3cf0924349cb11a119018e085a95a0a45c9660b9
js0.site <jssite@googlegroups.com>
2025-11-14 17:42:26 +0800

    mysql@0.1.29

--- dist.sh
+++ dist.sh
@@ -67,7 +67,7 @@ if [ -f "package.json" ]; then
   if [ -f "mod.node.js" ]; then
     mv mod.node.js mod.js
   fi
-  bun publish --access public --registry=https://registry.npmjs.org/
+  npm publish --access public --registry=https://registry.npmjs.org/
 fi
 
 # rm -rf $dist
