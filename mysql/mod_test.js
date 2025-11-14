f3d84c1fff39d8a269dfed6c0168521d1d386133
js0.site <jssite@googlegroups.com>
2025-11-15 07:36:53 +0800

    .

--- mysql/mod_test.js
+++ mysql/mod_test.js
@@ -4,3 +4,6 @@ import TIDB from "./conf/TIDB.js";
 import mysql from "./mod.js";
 
 const DB = mysql(TIDB);
+const hash = await DB.q00("SELECT ip FROM vps LIMIT 1");
+console.log(hash, typeof hash);
+process.exit(0);
