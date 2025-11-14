401f58a4ffa7f601f4ef7671fafda06895947b8f
js0.site <jssite@googlegroups.com>
2025-11-14 12:21:46 +0800

    mysql@0.1.19

--- mysql/confFromEnv.js
+++ mysql/confFromEnv.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env bun
 
-import parseEnv from "parse-dotenv";
+import { parseEnv } from "node:util";
+import read from "@3-/read";
 
-export default (env_path) =>
-  Object.fromEntries(
-    Object.entries(
-      parseEnv(env_path).map(([key, val]) => {
-        key = key.slice(6).toLocaleLowerCase();
+export default (env_path) => {
+  console.log(parseEnv(read(env_path)));
+  return Object.fromEntries(
+    Object.entries(parseEnv(read(env_path))).map(([key, val]) => {
+      key = key.slice(6).toLocaleLowerCase();
 
-        switch (key) {
-          case "pwd":
-            key = "password";
-            break;
-          case "ssl":
-            val = JSON.parse(val);
-            break;
-        }
-        return [key, val];
-      }),
-    ),
+      switch (key) {
+        case "pwd":
+          key = "password";
+          break;
+        case "ssl":
+          val = JSON.parse(val);
+          break;
+      }
+      return [key, val];
+    }),
   );
+};
