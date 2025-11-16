0f831385aa5b499da6a243f052fbfea434ca9972
js0.site <jssite@googlegroups.com>
2025-11-16 11:50:55 +0800

    .

--- send/mod.js
+++ send/mod.js
@@ -6,7 +6,7 @@ export default (conf) => {
     name_li = [];
 
   if (conf.PUSHPLUS) {
-    send_li.push(pushplus(conf.PUSHPLUS, conf.PUSHPLUS_TOPIC));
+    send_li.push(pushplus(...conf.PUSHPLUS));
     name_li.push("pushplus");
   }
   return Send(send_li, name_li);
