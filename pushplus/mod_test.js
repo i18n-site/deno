ab4598b1f939f77d898303ac819dcd7c4e1967cb
js0.site <jssite@googlegroups.com>
2025-11-16 11:31:31 +0800

    .

--- pushplus/mod_test.js
+++ pushplus/mod_test.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env bun
 
 import pushplus from "./mod.js";
-import { PUSHPLUS } from "../../../js0/conf/status/NOTIFY.js";
-console.log(PUSHPLUS);
+import { PUSHPLUS, PUSHPLUS_TOPIC } from "../../../js0/conf/status/NOTIFY.js";
+
+const send = pushplus(PUSHPLUS, PUSHPLUS_TOPIC);
+await send("测试", "正文");
