3f71bbf142876bcc5b31ddabb92c4acef70abd60
js0.site <jssite@googlegroups.com>
2025-11-16 11:48:51 +0800

    .

--- send/mod.js
+++ send/mod.js
@@ -1,8 +1,13 @@
 import Send from "./Send.js";
+import pushplus from "@8v/pushplus";
 
 export default (conf) => {
   const send_li = [],
     name_li = [];
 
+  if (conf.PUSHPLUS) {
+    send_li.push(pushplus(conf.PUSHPLUS, conf.PUSHPLUS_TOPIC));
+    name_li.push("pushplus");
+  }
   return Send(send_li, name_li);
 };
