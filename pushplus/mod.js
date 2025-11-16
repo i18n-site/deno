67fedbc3628799f934ab716e3c8a7f39ac28e26d
js0.site <jssite@googlegroups.com>
2025-11-16 11:09:50 +0800

    .

--- pushplus/mod.js
+++ pushplus/mod.js
@@ -1,7 +1,21 @@
 import curl from "@8v/curl/cJson";
 
-export default (token, topic) =>
-  async (title, text = "", url = "") => {
-    const url =
-      "https://www.pushplus.plus/send?token=31625fb059434c4983fdb50ddd90397a&title=XXX&content=XXX&template=html&topic=XXX";
-  };
+token 是 body 无 用户令牌或消息令牌
+title 否 body 无 消息标题
+content 是 body 无 具体消息内容，根据不同template支持不同格式
+template 否 body html 发送消息模板
+topic 否 body 无 群组编码
+channel 否 body wechat 发送渠道
+webhook 否 body 无 webhook编码
+callbackUrl 否 body 无 回调地址，异步回调发送结果
+timestamp 否 body 无 时间戳，毫秒。如小于当前时间，消息将无法发送
+
+const URL = 'https://www.pushplus.plus/send'
+
+export default (token, topic) => {
+  const conf = {
+    token,
+    topic
+  }
+  return async (title, text = "", url = "") => {};
+};
