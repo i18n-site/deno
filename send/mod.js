4625a3747a71eac0ef92c54f80324144596aebe4
js0.site <jssite@googlegroups.com>
2025-11-16 11:52:42 +0800

    .

--- send/mod.js
+++ send/mod.js
@@ -1,10 +1,14 @@
 import Send from "./Send.js";
 import pushplus from "@8v/pushplus";
+import lark from "@8v/lark";
 
 export default (conf) => {
   const send_li = [],
     name_li = [];
-
+  if (conf.LARK) {
+    send_li.push(lark(conf.LARK));
+    name_li.push("lark");
+  }
   if (conf.PUSHPLUS) {
     send_li.push(pushplus(...conf.PUSHPLUS));
     name_li.push("pushplus");
