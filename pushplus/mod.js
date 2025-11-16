6f92a02ae562cf69f8aece563fc06d821606139b
js0.site <jssite@googlegroups.com>
2025-11-16 11:07:01 +0800

    .

--- pushplus/mod.js
+++ pushplus/mod.js
@@ -1,1 +1,7 @@
-export default () => {};
+import curl from "@8v/curl/cJson";
+
+export default (token, topic) =>
+  async (title, text = "", url = "") => {
+    const url =
+      "https://www.pushplus.plus/send?token=31625fb059434c4983fdb50ddd90397a&title=XXX&content=XXX&template=html&topic=XXX";
+  };
